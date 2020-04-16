4740b2904ea56886f380a376847d819199e47d4b
octavian <coctavian10@yahoo.ro>
2020-04-16 15:46:05 +0300

    Lab 9

---
+++ Lab9/src/main/java/ro/uaic/info/repo/AlbumRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.uaic.info.repo;
+
+import ro.uaic.info.entity.Album;
+
+/**
+ *
+ * @author octavian
+ */
+public class AlbumRepository {
+    public void create(Album obj) {}
+    public Album findById(int id) { return null; }
+    public Album findByName(String name) { return null; }
+}
---
+++ Lab9/src/main/java/ro/uaic/info/repo/ArtistRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.uaic.info.repo;
+
+import ro.uaic.info.entity.Artist;
+
+/**
+ *
+ * @author octavian
+ */
+public class ArtistRepository {
+    public void create(Artist obj) {}
+    public Artist findById(int id) { return null; }
+    public Artist findByName(String name) { return null; }
+}
